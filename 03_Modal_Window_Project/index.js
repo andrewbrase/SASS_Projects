aa7b6673df094bf557c5a095ede3da2bc2d3474c
Brase <brasea@wwt.com>
2022-08-29 21:03:05 -0500

    working on modal

--- 03_Modal_Window_Project/index.js
+++ 03_Modal_Window_Project/index.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModel = document.querySelector('.close-modal');
+const buttonsOpenModel = document.querySelectorAll('.main__modal-button');
+
+for (let i = 0; i < buttonsOpenModel.length; i++) {
+    buttonsOpenModel[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+    });
+}
