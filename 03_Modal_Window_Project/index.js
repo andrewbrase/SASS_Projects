0950f36c9f8f042a92ff1ac999bf95515bf287fa
Brase <brasea@wwt.com>
2022-08-31 19:08:30 -0500

    worked on refactoring code

--- 03_Modal_Window_Project/index.js
+++ 03_Modal_Window_Project/index.js
@@ -5,14 +5,18 @@ const overlay = document.querySelector('.overlay');
 const buttonCloseModel = document.querySelector('.close-modal');
 const buttonsOpenModel = document.querySelectorAll('.main__modal-button');
 
-for (let i = 0; i < buttonsOpenModel.length; i++) {
-    buttonsOpenModel[i].addEventListener('click', function () {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-}
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
-buttonCloseModel.addEventListener('click', function () {
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+};
+
+for (let i = 0; i < buttonsOpenModel.length; i++) {
+    buttonsOpenModel[i].addEventListener('click', openModal) }
+
+buttonCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
