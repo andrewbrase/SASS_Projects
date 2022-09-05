3d5d84a339c78124ac7ab3f6369966d15fe36612
Brase <brasea@wwt.com>
2022-09-04 21:02:12 -0500

    fixed issue accepting numbers as guess

--- 04_hangman_project/index.js
+++ 04_hangman_project/index.js
@@ -12,18 +12,27 @@ const guess = document.querySelector('#guess');
 const submit = document.querySelector('#submit');
 const usedLetters = document.querySelector('#wrong');
 const remainGuesses = document.querySelector('#guessesnum');
+const hintword = document.querySelector('#hint');
 
-// make JSON obj that includes hints too?
+// this array will be the words that the user will be trying to guess
 var wordArr = ['Website', 'December', 'Cat', 'Lizard', 'Lemon', 'frog'];
+
+// this array will hold the hints to each of the guess words
+var hintlist = ['You could visit one of these on the web', 'Usually a very cold month', "A dog's foe", "One of Andrew's favorite Animals", 'Freshly squeezed', 'Enjoys hopping'];
+
+// this array includes all of the body parts of the stick man
 var character = [head, leftArm, torso, rightArm, leftLeg, rightLeg];
 
+// this is the used letters area, if a letter is guessed - it will be added to this area
 usedLetters.textContent = '';
 
+// this function will remove the hidden class from the body part and it will be used if the player guesses a wrong letter
 const addBodyPart = (part) => {
     part.classList.remove('hidden');
     bodyIndex++
 }
 
+// this will hide the stick man by adding the hidden class to each body part
 const reHideBody = () => {
     for (let i = 0; i < character.length; i++){
         character[i].classList.add('hidden');
@@ -32,8 +41,11 @@ const reHideBody = () => {
 
 // this is the index of the array of words to guess, a new round will up-tick this
 var wordArrIndex = 0;
+
+// this is the index of the stick figure part to reveal if an incorrect guess
 var bodyIndex = 0;
 
+// this function will create _ spaces for each letter of the guess word
 const createSpaces = (array, word) => {
     let spaces = '';
     for (let i = 0; i < array[word].length; i++) {
@@ -42,20 +54,26 @@ const createSpaces = (array, word) => {
     goalWord.textContent = spaces;
 }
 
+// initial spaces are created, the function above can be called again
 var spaces = '';
-
 for (let i = 0; i < wordArr[wordArrIndex].length; i++) {
     spaces += '_'
 }
 
+// hiding the goal word with the _ spaces
 goalWord.textContent = spaces;
 
+// split of goal word
 var newArr = goalWord.textContent.split('')
 
+// this event listener will listen for the player guess
 submit.addEventListener('click', function () {
 
-    if (guess.value !== '') {
+    // check to see if the input is a number
+    var matches = guess.value.match(/\d+/g);
+    if (guess.value !== '' && matches === null) {
 
+        // this will search every letter of the guess word for the player guess
         for (let i = 0; i < wordArr[wordArrIndex].length; i++) {
             
             if (wordArr[wordArrIndex][i].toUpperCase() === guess.value.toUpperCase()) {
@@ -65,19 +83,24 @@ submit.addEventListener('click', function () {
             }
         }
 
+        // if the player guess is not in the goal word - a stick figure part will be added and the remaining guesses will be lowered
         if (goalWord.textContent.includes(guess.value) == false) {
             addBodyPart(character[bodyIndex])
+            remainGuesses.textContent = String(parseInt(remainGuesses.textContent) - 1)
         }
 
+        // adding the player guess to the guessed letters field
         usedLetters.textContent = usedLetters.textContent + ' ' + guess.value
-        remainGuesses.textContent = String(parseInt(remainGuesses.textContent) - 1)
 
+        // if the submit button was pressed without a guess
     } else {
         alert('Please enter a valid letter a-z');
     }
 
+    // resetting the guess value so that a new guess can be entered
     guess.value = '';
-    // need to downtick remaining guesses
+    
+    // checks for victory condition - also if the player has lost the game
     if (!goalWord.textContent.includes('_')) {
         setTimeout(() => {
             alert('You win!')
@@ -90,6 +113,12 @@ submit.addEventListener('click', function () {
 
 })
 
+// will alert the player of the guess for the word
+hintword.addEventListener('click' , function () {
+    alert(hintlist[wordArrIndex])
+})
+
+// this will reset the game with a new word and reset the score
 newGameButton.addEventListener('click' , function () {
     remainGuesses.textContent = '6'
     usedLetters.textContent = ''
