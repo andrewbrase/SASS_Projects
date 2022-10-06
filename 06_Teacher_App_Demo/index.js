c5e1cf28b3ada0d2bdea6797fe14ae0b0650ac84
Brase <brasea@wwt.com>
2022-10-06 11:00:15 -0500

    started working on teacher app

--- 06_Teacher_App_Demo/index.js
+++ 06_Teacher_App_Demo/index.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const newStudentButton = document.getElementById('new-student-button');
-const modal = document.getElementById('modal')
+const modal = document.getElementById('modal');
 const closeModalButton = document.getElementById('close-modal-button');
-const modalText = document.getElementById('modal-text')
+const modalText = document.getElementById('modal-text');
+const unassignedStudents = document.getElementById('unassigned-students');
+const classMap = new Map();
 
 const toggleModal = () => {
 
-    modal.classList.toggle('hidden')
+    modal.classList.toggle('hidden');
 
 }
 
@@ -26,8 +28,32 @@ const addStudentRoster = () => {
     let submitButton = document.getElementById('submit-button')
     submitButton.addEventListener('click', function submitHandler(){
         let newStudentFirstName = document.getElementById('firstname').value
-        console.log(newStudentFirstName)
-        toggleModal()
+        let newStudentLastName = document.getElementById('lastname').value
+        let newStudentAge = document.getElementById('age').value
+
+        if (newStudentFirstName !== '' && newStudentLastName !== '' && newStudentAge !== ''){
+
+            // these are the values that were assigned from the input fields
+            // console.log(`new student ${newStudentFirstName} , ${newStudentLastName} age: ${newStudentAge} will be added to roster`)
+            toggleModal()
+
+            // add student to the map? will use this feature later
+            // classMap.set('newStudentFirstName', {lastname : newStudentLastName, age :newStudentAge})
+            // console.log(classMap.get('newStudentFirstName'))
+
+            // add student to roster
+            let studentTest = `<p id="P${newStudentFirstName}">${newStudentFirstName} ${newStudentLastName} age: ${newStudentAge} <button id="B${newStudentFirstName}">Assign -></button></p>`
+            unassignedStudents.insertAdjacentHTML('beforeend', studentTest);
+
+            // adds a submit button event handler per student
+            let newStudentAssignButton = document.getElementById(`B${newStudentFirstName}`);
+            newStudentAssignButton.addEventListener('click' , function test(){
+                console.log('test')
+            })
+
+        } else {
+            alert('Please fill in every field');
+        }
         
     })
 
